0a3bde4a758651eb80b5610201b2252fdfc229a6
Belar <belar@belardesign.com>
2021-05-22 15:02:57 +0200

    Add panel header

--- src/contacts-panel/SearchList.tsx
+++ src/contacts-panel/SearchList.tsx
@@ -16,11 +16,18 @@ export interface Props {
 }
 
 const Panel = styled.div`
+  --bleachWhite: #ffffff;
+  --graphiteBlack: #181b2e;
+  --fadeGrey: #f4f6fa;
+  --elephantGrey: #c4cedc;
+  --inkBlue: #415f8c;
+  --simpleBlue: #327be8;
+
   font-family: sans-serif;
 
   box-sizing: border-box;
 
-  background: #ffffff;
+  background: var(--bleachWhite);
   max-width: 450px;
   margin: auto;
   padding: 45px 45px 40px;
@@ -29,6 +36,57 @@ const Panel = styled.div`
   box-shadow: 0px 15px 45px -25px rgba(1, 39, 97, 0.19);
 `;
 
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Heading = styled.h3`
+  margin: 0;
+  font-size: 18px;
+  color: var(--graphiteBlack);
+`;
+
+const Close = styled.button`
+  background: none;
+  border: none;
+
+  cursor: pointer;
+
+  margin-left: auto;
+  padding: 0;
+`;
+
+const CloseIcon = styled.svg`
+  width: 44px;
+  height: 44px;
+
+  color: var(--inkBlue);
+`;
+
 export function SearchList(props: Props): React.ReactElement {
-  return <Panel></Panel>;
+  const { title } = props;
+
+  return (
+    <Panel>
+      <Header>
+        <Heading>{title}</Heading>
+        <Close>
+          <CloseIcon
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </CloseIcon>
+        </Close>
+      </Header>
+    </Panel>
+  );
 }
